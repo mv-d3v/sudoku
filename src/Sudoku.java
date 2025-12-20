d3513ebef8bd4187eae70d09906b1f722c634fb9
Marcos Vinicius <marcos.vinicius.alves.souza@outlook.com>
2025-12-20 15:46:03 -0300

    Issues #9 and #10 resolved in this commit.

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
 import java.util.List;
 
 public class Sudoku {
+    private int[][] sudokuOriginalTable = new int[9][9];
     private final int[][] sudokuTable = new int[9][9];
 
     public Sudoku(List<String> numbers) {
@@ -12,15 +14,17 @@ public class Sudoku {
             int number = Integer.parseInt(positionSplited[2]);
 
             this.sudokuTable[row][column] = number;
+            this.sudokuOriginalTable[row][column] = number;
         }
+
     }
 
     private boolean verifySudokuPosition(int row, int column) {
-        boolean isAvailablePosition = this.sudokuTable[row][column] == 0;
+        boolean isAvailablePosition = this.sudokuOriginalTable[row][column] == 0;
         return isAvailablePosition;
     }
 
-    private boolean verifyIfNumberAvailableInArrayRange(int row, int column, int number) {
+    private boolean isNumberAvailableQuadrant(int row, int column, int number) {
         int blockRowNumber = row/3;
         int blockColNumber = column/3;
 
@@ -60,16 +64,31 @@ public class Sudoku {
     }
 
     public void showSudokuTable() {
-        System.out.printf("    0 1 2 3 4 5 6 7 8\n");
-        System.out.println();
+        System.out.printf("    0  1  2   3  4  5   6  7  8\n");
+        System.out.printf("  -------------------------------\n");
 
+        int columnCountToFormat = 0;
+        int rowCountToFormat = 0;
         for(int row = 0; row != 9; row++) {
+            if(rowCountToFormat == 3) {
+                rowCountToFormat = 0;
+                System.out.printf("  |-----------------------------|\n");
+            }
+            rowCountToFormat += 1;
+
             for(int column = 0; column != 9; column++) {
-                if(column == 0) {System.out.printf("%s   ", row);}
-                System.out.printf("%s ", sudokuTable[row][column]);
+                if(column == 0) {System.out.printf("%s |", row);}
+                System.out.printf(" %s ", sudokuTable[row][column]);
+                columnCountToFormat += 1;
+
+                if(columnCountToFormat == 3) {
+                    System.out.print("|");
+                    columnCountToFormat = 0;
+                }
             }
             System.out.println();
         }
+        System.out.printf("  -------------------------------\n");
     }
 
     public void insertNumberInPosition(String userRow, String userColumn, String userNumber) {
@@ -78,12 +97,10 @@ public class Sudoku {
             int column = Integer.parseInt(userColumn);
             int number = Integer.parseInt(userNumber);
 
-            if(!this.verifyIfNumberAvailableInArrayRange(row, column, number)) {
-                System.out.println(">> Posição não disponível, selecione outra posição. <<");
-            } else if(!this.isColumnAndRowAvailable(row, column, number)) {
-                System.out.println(">> Número já está presente no quadrante selecionado. <<");
+            if(!this.isNumberAvailableQuadrant(row, column, number) || !this.isColumnAndRowAvailable(row, column, number)) {
+                System.out.println(">> Posição indisponível, selecione outra posição. <<");
             } else if(!this.verifySudokuPosition(row, column)) {
-                System.out.println(">> Número já está presente na linha ou coluna. <<");
+                System.out.println(">> Já existe um número ocupando esta posição. <<");
             } else {
                 this.sudokuTable[row][column] = number;
                 this.showSudokuTable();
